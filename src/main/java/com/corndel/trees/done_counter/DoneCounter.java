8619ba005337176fde32bbe2e56c512793c12641
john <mustchjo@tutanota.com>
2025-07-24 16:06:18 +0100

    workshop tasks

--- src/main/java/com/corndel/trees/done_counter/DoneCounter.java
+++ src/main/java/com/corndel/trees/done_counter/DoneCounter.java
@@ -2,14 +2,37 @@ package com.corndel.trees.done_counter;
 
 import com.corndel.trees.dto.Task;
 
+import java.util.Stack;
+
 public class DoneCounter {
 
     public static int countIterative(Task task) {
-        return 0;
+        int totalDone = 0;
+
+        Stack<Task> taskStack = new Stack<>();
+        taskStack.push(task);
+
+        while(!taskStack.isEmpty()){
+            Task currentNode = taskStack.pop();
+            if(currentNode.isDone()){
+                totalDone++;
+            }
+            for(Task child : currentNode.getChildren() ){
+                taskStack.push(child);
+            }
+        }
+        return totalDone;
     }
 
     public static int countRecursive(Task task) {
-        return 0;
+        int currentCount = 0;
+        if(task.isDone()){
+            currentCount++;
+        }
+        for(Task child : task.getChildren() ){
+            currentCount += countRecursive(child);
+        }
+        return currentCount;
     }
 
 }
